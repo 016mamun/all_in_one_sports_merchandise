65719aa89b61b925c143180373f47416ef989348
Mamun <wmamun690@gmail.com>
2025-04-09 03:18:57 +0600

    fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: ['https://all-in-one-sports-merchandise.vercel.app/', 'http://localhost:3000'],
+  origin: ['https://all-in-one-sports-merchandise.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
